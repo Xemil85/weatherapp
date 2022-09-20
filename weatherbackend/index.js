ce3e05f7af87da662664c88fd6a10c0b6d0a7bcb
Eemil Heiskanen <heiskaneneemil@gmail.com>
2022-09-20 12:55:08 +0300

    Added express static for fly.io and docker container frontend

--- weatherbackend/index.js
+++ weatherbackend/index.js
@@ -8,9 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static("dist"));
+
 app.use("/", saa);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 var server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
