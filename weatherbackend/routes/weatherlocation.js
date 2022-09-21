115e57efb1a7b85a5a78795953a85d7e0d6dd2f1
Eemil Heiskanen <heiskaneneemil@gmail.com>
2022-09-21 12:49:28 +0300

    Changed api urls for backend

--- weatherbackend/routes/weatherlocation.js
+++ weatherbackend/routes/weatherlocation.js
@@ -5,12 +5,17 @@ require("dotenv").config();
 
 router.post("/saa", async (req, res) => {
   const city = req.body.city ? req.body.city : "";
-  const apikey = process.env.WEATHERKEY
+  const apikey = process.env.WEATHERKEY;
   try {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&lang=fi&units=metric`);
-    //console.log(response.data);
-    console.log(response.data.weather[0]);
-    res.json(response.data);
+    const response = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apikey}`
+    );
+    console.log(response.data);
+    const responseWeather = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${response.data[0].lat}&lon=${response.data[0].lon}&appid=${apikey}&lang=fi&units=metric`
+    );
+    console.log(responseWeather.data);
+    res.json(responseWeather.data);
   } catch (err) {
     return console.log(err);
   }
